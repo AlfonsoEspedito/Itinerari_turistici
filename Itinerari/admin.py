ecbf00b06212d7600a8f2fdfdabb7f676f6d93b7
AlfonsoEspedito <alfonsoespedito@gmail.com>
2025-06-24 12:45:49 +0200

    modifiche html

--- Itinerari/admin.py
+++ Itinerari/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Lingua, Amministratore, Turista, Tappa, Guida, Itinerario, Feedback, Prenotazione
+from .models import Lingua, Amministratore, Turista, Tappa, Guida, Itinerario, Prenotazione
 
 admin.site.register(Lingua)
 admin.site.register(Amministratore)
@@ -8,5 +8,4 @@ admin.site.register(Turista)
 admin.site.register(Tappa)
 admin.site.register(Guida)
 admin.site.register(Itinerario)
-admin.site.register(Feedback)
 admin.site.register(Prenotazione)
